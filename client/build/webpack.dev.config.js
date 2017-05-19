953e8c2dadd85729c8cd18d1f57d6bfeea6d11fb
chenyihong <496695119@qq.com>
2017-05-19 16:45:13 +0800

    独立出babelrc文件，基本框架搭建完成，可以开始愉快地react了

--- client/build/webpack.dev.config.js
+++ client/build/webpack.dev.config.js
@@ -17,7 +17,10 @@ module.exports = {
             {
                test:/\.(js|jsx)$/,
                exclude:/node_modules/,
-               use: 'babel-loader'
+               use: {
+                   loader:'babel-loader',
+                   options:{}
+                }
             }
         ]
     }
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,1 +1,19 @@
-console.log('我是入口文件')
+import React from 'react'
+import reactDOM from 'react-dom'
+
+class App extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    componentWillMount(){
+        const o = {a:1};
+        console.log('测试ES7语法：成功则输出')
+        console.log({...o})
+    }
+    render(){
+        return (<div>该条数据来自客户端渲染</div>)
+    }
+}
+
+reactDOM.render(<App/>,document.getElementById('app'));
